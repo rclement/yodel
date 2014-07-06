dd28fd27477de274417d1af5b52490503e3b6f0b
Romain Clement <r.clement88@gmail.com>
2014-07-06 16:34:01 +0200

    [DEV] Add damn description in package docstring

--- damn/__init__.py
+++ damn/__init__.py
@@ -1,5 +1,6 @@
 """
-Digital Audio for Music Nerds
+**damn** (aka *digital audio for music nerds*) is an easy-to-use python package
+for digital audio signal processing, analysis and synthesis.
 """
 
 __version__ = '0.0.0'
