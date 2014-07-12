e43596395507c4606909087c0e77e84c1a232811
Romain Clement <r.clement88@gmail.com>
2014-07-12 11:00:53 +0200

    [DEV] Add meta information for damn package

--- damn/__init__.py
+++ damn/__init__.py
@@ -3,4 +3,11 @@
 for digital audio signal processing, analysis and synthesis.
 """
 
-__version__ = '0.0.0'
+__author__ = 'Romain Clement'
+__copyright__ = 'Copyright 2014, Romain Clement'
+__credits__ = []
+__license__ = 'MIT'
+__version__ = "0.0.0"
+__maintainer__ = 'Romain Clement'
+__email__ = 'contact@romainclement.com'
+__status__ = 'Development'
