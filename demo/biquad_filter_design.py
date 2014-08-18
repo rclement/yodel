8b8564dce458a796e25477c834638aececfaa10d
Romain Clement <r.clement88@gmail.com>
2014-08-18 20:20:28 +0200

    [DEMO] Update biquad filter design demo with phase view instead of linear amplitude

--- demo/biquad_filter_design.py
+++ demo/biquad_filter_design.py
@@ -24,7 +24,7 @@ def frequency_response(response):
     return freq_response_real, freq_response_imag
 
 
-def amplitude_response(spec_real, spec_imag, db = False):
+def amplitude_response(spec_real, spec_imag, db=True):
     size = len(spec_real)
     amp = [0] * size
     for i in range(0, size):
@@ -34,22 +34,31 @@ def amplitude_response(spec_real, spec_imag, db = False):
     return amp
 
 
+def phase_response(spec_real, spec_imag, degrees=True):
+    size = len(spec_real)
+    pha = [0] * size
+    for i in range(0, size):
+        pha[i] = yodel.complex.phase(spec_real[i], spec_imag[i])
+        if degrees:
+            pha[i] = (pha[i] * 180.0 / math.pi)
+    return pha
+
+
 class BiquadSelector:
     def __init__(self):
         self._bq_fs = 48000
         self._bq_fc = self._bq_fs/4
-        self._bq_q  = 1
+        self._bq_q  = 1.0 / math.sqrt(2.0)
         self._bq_dbgain = 0
         self._bq_size = 512
-        self._bq_plot_db = False
+        self._bq_plot_db = True
         self._nfft = int(self._bq_size / 2)
         self._impulse_response = [0] * self._bq_size
         self._freq_response_real = [0] * self._bq_size
         self._freq_response_imag = [0] * self._bq_size
-        self._amplitude_response = [0] * self._bq_size
-        self._phase_response = [0] * self._bq_size
+        self._response = [0] * self._bq_size
         self._bq_filter = yodel.filter.Biquad()
-        self._biquad_type = 'LPF'
+        self._biquad_type = 'Low Pass'
         self.update_biquad()
 
         self._create_plot()
@@ -63,31 +72,31 @@ class BiquadSelector:
 
         self.update_biquad_response()
         self._x_axis = [i*(self._bq_fs/2/self._nfft) for i in range(0, self._nfft)]
-        self._y_axis = self._amplitude_response[0:self._nfft]
+        self._y_axis = self._response[0:self._nfft]
 
         if self._bq_plot_db:
             self._l_bot, = self._ax.plot(self._x_axis, [-100] * self._nfft, 'k')
             self._l_top, = self._ax.plot(self._x_axis, [0] * self._nfft, 'k')
         else:
-            self._l_bot, = self._ax.plot(self._x_axis, [0] * self._nfft, 'k')
-            self._l_top, = self._ax.plot(self._x_axis, [1] * self._nfft, 'k')
+            self._l_bot, = self._ax.plot(self._x_axis, [- 180] * self._nfft, 'k')
+            self._l_top, = self._ax.plot(self._x_axis, [180] * self._nfft, 'k')
 
         if self._bq_plot_db:
             self._l_fc, = self._ax.plot([self._bq_fc] * 100, [i for i in range(-100, 0)], 'k')
         else:
-            self._l_fc, = self._ax.plot([self._bq_fc] * self._nfft, [i/self._nfft for i in range(0, self._nfft)], 'k')
+            self._l_fc, = self._ax.plot([self._bq_fc] * int(2.0 * self._nfft), [(180*i/self._nfft) for i in range(-self._nfft, self._nfft)], 'k')
 
         self._l_fr, = self._ax.plot(self._x_axis, self._y_axis, 'b')
 
         self._rescale_plot()
 
     def _create_plot_controls(self):
-        self._dbrax = plt.axes([0.12, 0.05, 0.10, 0.10])
-        self._dbradio = RadioButtons(self._dbrax, ('Linear', 'dB'))
+        self._dbrax = plt.axes([0.12, 0.05, 0.13, 0.10])
+        self._dbradio = RadioButtons(self._dbrax, ('Amplitude', 'Phase'))
         self._dbradio.on_clicked(self.set_plot_style)
 
-        self._rax = plt.axes([0.25, 0.03, 0.15, 0.20])
-        self._radio = RadioButtons(self._rax, ('LPF', 'HPF', 'BPF', 'APF', 'Notch', 'Peak', 'LowShelf', 'HighShelf'))
+        self._rax = plt.axes([0.27, 0.03, 0.15, 0.20])
+        self._radio = RadioButtons(self._rax, ('Low Pass', 'High Pass', 'Band Pass', 'All Pass', 'Notch', 'Peak', 'Low Shelf', 'High Shelf'))
         self._radio.on_clicked(self.set_biquad_type)
 
         self._sfax = plt.axes([0.6, 0.19, 0.2, 0.03])
@@ -102,21 +111,21 @@ class BiquadSelector:
         self._dbslider.on_changed(self.set_biquad_dbgain)
 
     def update_biquad(self):
-        if self._biquad_type == 'LPF':
+        if self._biquad_type == 'Low Pass':
             self._bq_filter.low_pass(self._bq_fs, self._bq_fc, self._bq_q)
-        elif self._biquad_type == 'HPF':
+        elif self._biquad_type == 'High Pass':
             self._bq_filter.high_pass(self._bq_fs, self._bq_fc, self._bq_q)
-        elif self._biquad_type == 'BPF':
+        elif self._biquad_type == 'Band Pass':
             self._bq_filter.band_pass(self._bq_fs, self._bq_fc, self._bq_q)
-        elif self._biquad_type == 'APF':
+        elif self._biquad_type == 'All Pass':
             self._bq_filter.all_pass(self._bq_fs, self._bq_fc, self._bq_q)
         elif self._biquad_type == 'Notch':
             self._bq_filter.notch(self._bq_fs, self._bq_fc, self._bq_q)
         elif self._biquad_type == 'Peak':
             self._bq_filter.peak(self._bq_fs, self._bq_fc, self._bq_q, self._bq_dbgain)
-        elif self._biquad_type == 'LowShelf':
+        elif self._biquad_type == 'Low Shelf':
             self._bq_filter.low_shelf(self._bq_fs, self._bq_fc, self._bq_q, self._bq_dbgain)
-        elif self._biquad_type == 'HighShelf':
+        elif self._biquad_type == 'High Shelf':
             self._bq_filter.high_shelf(self._bq_fs, self._bq_fc, self._bq_q, self._bq_dbgain)
 
     def set_biquad_type(self, biquad_type):
@@ -140,22 +149,18 @@ class BiquadSelector:
         self.update_biquad()
         self._plot_frequency_response()
 
-    # def set_biquad_parameters(self, fc, q, dbgain):
-    #     self._bq_fc = fc
-    #     self._bq_q = q
-    #     self._bq_dbgain = dbgain
-    #     self.update_biquad()
-    #     self._plot_frequency_response()
-
     def update_biquad_response(self):
         self._impulse_response = impulse_response(self._bq_filter, self._bq_size)
         self._freq_response_real, self._freq_response_imag = frequency_response(self._impulse_response)
-        self._amplitude_response = amplitude_response(self._freq_response_real, self._freq_response_imag, self._bq_plot_db)
+        if self._bq_plot_db:
+            self._response = amplitude_response(self._freq_response_real, self._freq_response_imag)
+        else:
+            self._response = phase_response(self._freq_response_real, self._freq_response_imag)
 
     def set_plot_style(self, style):
-        if style == 'Linear':
+        if style == 'Phase':
             self._bq_plot_db = False
-        elif style == 'dB':
+        elif style == 'Amplitude':
             self._bq_plot_db = True
         self._plot_range_limits(False)
         self._plot_frequency_response(False)
@@ -163,13 +168,13 @@ class BiquadSelector:
 
     def _plot_frequency_response(self, redraw = True):
         self.update_biquad_response()
-        self._y_axis = self._amplitude_response[0:self._nfft]
+        self._y_axis = self._response[0:self._nfft]
         if self._bq_plot_db:
             self._l_fc.set_xdata([self._bq_fc] * 100)
             self._l_fc.set_ydata([i for i in range(-100, 0)])
         else:
-            self._l_fc.set_xdata([self._bq_fc] * self._nfft)
-            self._l_fc.set_ydata([i/self._nfft for i in range(0, self._nfft)])
+            self._l_fc.set_xdata([self._bq_fc] * int(2.0 * self._nfft))
+            self._l_fc.set_ydata([(180*i/self._nfft) for i in range(-self._nfft, self._nfft)])
         self._l_fr.set_ydata(self._y_axis)
         if redraw:
             plt.draw()
@@ -179,16 +184,16 @@ class BiquadSelector:
             self._l_bot.set_ydata([-100] * self._nfft)
             self._l_top.set_ydata([0] * self._nfft)
         else:
-            self._l_bot.set_ydata([0] * self._nfft)
-            self._l_top.set_ydata([1] * self._nfft)
+            self._l_bot.set_ydata([- 180] * self._nfft)
+            self._l_top.set_ydata([180] * self._nfft)
         if redraw:
             plt.draw()
 
     def _rescale_plot(self):
         if self._bq_plot_db:
-            self._ax.set_ylim(-100, 20)
+            self._ax.set_ylim(-110, 20)
         else:
-            self._ax.set_ylim(0, 1.5)
+            self._ax.set_ylim(- 200, 200)
         plt.draw()
 
 
