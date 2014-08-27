de608e2d35f2967ebba11d1d061c0b45e013b121
Romain Clement <r.clement88@gmail.com>
2014-08-27 23:34:30 +0200

    [DEV] Fix variable naming convention for complex module

--- yodel/complex.py
+++ yodel/complex.py
@@ -5,23 +5,23 @@ This module provides utility functions for complex numbers.
 import math
 
 
-def modulus(re, im):
+def modulus(real, imag):
     """
     Compute the modulus of a complex number.
 
-    :param re: real part of the complex number
-    :param im: imaginary part of the complex number
+    :param real: real part of the complex number
+    :param imag: imaginary part of the complex number
     :rtype: modulus of complex number
     """
-    return math.sqrt(re * re + im * im)
+    return math.sqrt(real * real + imag * imag)
 
 
-def phase(re, im):
+def phase(real, imag):
     """
     Compute the phase of a complex number.
 
-    :param re: real part of the complex number
-    :param im: imaginary part of the complex number
+    :param real: real part of the complex number
+    :param imag: imaginary part of the complex number
     :rtype: phase of complex number
     """
-    return math.atan2(im, re)
+    return math.atan2(imag, real)
